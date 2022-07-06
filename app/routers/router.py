3a6f325e947092bb0b36f00c3effcbc054a96d4d
Denis Kovshun <deniskovshun@gmail.com>
2022-07-06 17:41:53 +0300

    Code refactoring

--- app/routers/router.py
+++ app/routers/router.py
@@ -7,19 +7,21 @@ router = APIRouter()
 
 
 @router.get('/delivery-list', status_code=status.HTTP_200_OK)
-async def get_delivery(lng: float = 28.66468, lat: float = 50.26009, conn = Depends(db.connection)):
+async def get_delivery(lng: float = 28.66468, lat: float = 50.26009, conn=Depends(db.connection)):
 
     sql_q = """SELECT * FROM cp_GetNearestOrders($1, $2)"""
 
-    res = await conn.fetchval(sql_q, lng, lat)
-
-    if not res:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return res
+    try:
+        res = await conn.fetchval(sql_q, lng, lat)
+        if not res:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return res
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @router.get('/directions', status_code=status.HTTP_200_OK)
-async def get_directions(origin: str = '28.66468,50.26009', destination: str = '30.11233,49.79832', conn = Depends(db.connection)):
+async def get_directions(origin: str = '28.66468,50.26009', destination: str = '30.11233,49.79832', conn=Depends(db.connection)):
 
     try:
         lng1, lat1 = [float(x.strip()) for x in origin.split(',')]
@@ -28,20 +30,25 @@ async def get_directions(origin: str = '28.66468,50.26009', destination: str = '
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
     sql_q = """SELECT * FROM cp_FromAtoB($1, $2, $3, $4)"""
-    res = await conn.fetchval(sql_q, lng1, lat1, lng2, lat2)
 
-    if not res:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return res
+    try:
+        res = await conn.fetchval(sql_q, lng1, lat1, lng2, lat2)
+        if not res:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return res
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @router.post('/tcp-route', status_code=status.HTTP_200_OK)
-async def get_tcp(q: schemas.RouteQuery, conn = Depends(db.connection)):
+async def get_tcp(q: schemas.RouteQuery, conn=Depends(db.connection)):
 
     sql_q = """SELECT * FROM gihcp_TCProute($1, $2, $3, $4)"""
 
-    res = await conn.fetchval(sql_q, q.longitude, q.latitude, q.storage_ids, q.order_ids)
-
-    if not res:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    return res
+    try:
+        res = await conn.fetchval(sql_q, q.longitude, q.latitude, q.storage_ids, q.order_ids)
+        if not res:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        return res
+    except:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
